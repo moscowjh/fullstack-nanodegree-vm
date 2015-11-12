866c56a599f97ddf9fcdbadcba709d5c0d1c935b
moscowjh <horowitz.jason@gmail.com>
2015-11-12 09:49:59 -0500

    Add initial SQL statements and other experiments.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,26 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+\c tournament;
+DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS matches;
 
+CREATE TABLE players ( id serial primary key,name text);
+
+CREATE TABLE matches (
+  id serial primary key,
+  winner int REFERENCES players(id)
+  ON DELETE CASCADE,
+  loser int REFERENCES players(id)
+  ON DELETE CASCADE
+);
+
+INSERT INTO players VALUES (default,'John');
+INSERT INTO players VALUES (default,'Paul');
+INSERT INTO players VALUES (default,'George');
+INSERT INTO players VALUES (default,'Ringo');
+INSERT INTO players VALUES (default,'Mick');
+INSERT INTO players VALUES (default,'Keith');
+INSERT INTO players VALUES (default,'Brian');
+INSERT INTO players VALUES (default,'Bill');
+INSERT INTO players VALUES (default,'Charlie');
